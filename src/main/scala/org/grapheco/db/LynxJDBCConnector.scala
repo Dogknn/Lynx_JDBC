c17be341995f532021f8b74401a659f363a79918
Dogknn <1958838894@qq.com>
2024-10-22 09:03:58 +0800

    rewrite j

--- src/main/scala/org/grapheco/db/LynxJDBCConnector.scala
+++ src/main/scala/org/grapheco/db/LynxJDBCConnector.scala
@@ -8,7 +8,6 @@ import schema.{Schema, SchemaManager}
 class LynxJDBCConnector {
   object LynxJDBCConnector {
 
-    // 带 schema 的连接方法
     def connect(url: String, username: String, password: String, schema: Schema): LynxJDBCConnector = {
       val db: DB = new DB(url, username, password)
       val graphModel: JDBCGraphModel = new JDBCGraphModel(db.connection, schema)
@@ -16,7 +15,6 @@ class LynxJDBCConnector {
       new LynxJDBCConnector(graphModel, runner, schema)
     }
 
-    // 自动生成 schema 的连接方法
     def connect(url: String, username: String, password: String): LynxJDBCConnector = {
       val db: DB = new DB(url, username, password)
       val schema: Schema = SchemaManager.autoGeneration(db.connection)
@@ -25,11 +23,7 @@ class LynxJDBCConnector {
       new LynxJDBCConnector(graphModel, runner, schema)
     }
   }
-
-  // 数据库连接器类
   case class LynxJDBCConnector(graphModel: GraphModel, runner: CypherRunner, schema: Schema) {
-
-    // 运行查询
     def run(query: String, param: Map[String, Any] = Map.empty): LynxResult = runner.run(query, param)
   }
 
